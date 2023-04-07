16c1f0f1e406747f595a7bc20ecda870a4faae04
Carlos Tovar <c.tovarb@uniandes.edu.co>
2023-04-06 23:26:49 -0500

    feat: uploading files

---
+++ app.py
@@ -0,0 +1,87 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_restful import Api
+
+from modelos import db
+from vistas import VistaSignIn, VistaLogIn, VistaTasks, VistaTaskId, VistaFiles, VistaUsuarios, VistaUsuario
+
+UPLOAD_FOLDER = 'C:\tmp\Sist_Conv_Files'
+app = Flask(__name__)
+
+app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sistema_conversion.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# el JWT_SECRET_KEY se debe modificar?
+app.config['JWT_SECRET_KEY'] = 'frase-secreta'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+app_context = app.app_context()
+app_context.push()
+
+db.init_app(app)
+db.create_all()
+
+jwt = JWTManager(app)
+# validar si esto se necesita
+cors = CORS(app)
+
+api = Api(app)
+api.add_resource(VistaSignIn, '/api/auth/signup')
+api.add_resource(VistaLogIn, '/api/auth/login')
+api.add_resource(VistaTasks, '/api/tasks')
+api.add_resource(VistaTaskId, '/api/tasks/<int:id_task>')
+api.add_resource(VistaFiles, '/api/files/<string:filename>')
+api.add_resource(VistaUsuarios, '/usuarios')
+api.add_resource(VistaUsuario, '/usuario/<int:id_usuario>')
+
+if __name__ == '__main__':
+    app.run()
+
+# #Prueba
+# with app.app_context():
+#     u = Usuario(usuario_nombre='UserTest', contrasena='123456', correo='testuser@gmail.com')
+#     db.session.add(u)
+#     db.session.commit()
+#     print (Usuario.query.all())
+
+# Prueba
+# with app.app_context():
+#     u = Usuario(usuario_nombre='UserTest', contrasena='123456', correo='testuser@gmail.com')
+#     a = Archivo(nombre_original = 'PrimerArchivo', archivo_original="Archivo guardado", fecha_creacion= datetime(2022,4,2), estado_conversion=EstadoConversion.UPLOADED, usuario_id=1)
+#     t =TareaConversion(extension_final=ExtensionFinal.ZIP,estado_tarea=EstadoTarea.DISPONIBLE,usuario_id=1,archivo_id=1)
+#     #t.archivos.append(a) // Esta no funciono!!
+#     u.tareas.append(t)
+#     db.session.add(u)
+#     db.session.add(t)
+#     db.session.add(a)
+#     db.session.commit()
+#     print (Usuario.query.all())
+#     print (Usuario.query.all()[0].tareas)
+#     print (TareaConversion.query.all())
+#     db.session.delete(t)
+#     print (Usuario.query.all())
+#     print (TareaConversion.query.all())
+
+# Prueba
+# with app.app_context():
+#     user_schema=UsuarioSchema()
+#     U =Usuario(usuario_nombre='UserTest', contrasena='123456', correo='testuser@gmail.com')
+#     db.session.add(U)
+#     db.session.commit()
+#     print([user_schema.dumps(usuario) for usuario in Usuario.query.all()])
+
+# with app.app_context():
+#     tarea_schema=TareaConversionSchema()
+#     T =TareaConversion(extension_final=ExtensionFinal.ZIP,estado_tarea=EstadoTarea.DISPONIBLE)
+#     db.session.add(T)
+#     db.session.commit()
+#     print([tarea_schema.dumps(tarea) for tarea in TareaConversion.query.all()])
+
+# with app.app_context():
+#     archivo_schema=ArchivoSchema()
+#     A = Archivo(nombre_original = 'PrimerArchivo', archivo_original="Archivo guardado", fecha_creacion= datetime(2022,4,2), estado_conversion=EstadoConversion.UPLOADED)
+#     db.session.add(A)
+#     db.session.commit()
+#     print([archivo_schema.dumps(archivo) for archivo in Archivo.query.all()])
---
+++ vistas/vistas.py
@@ -0,0 +1,115 @@
+from flask import request
+from flask_jwt_extended import jwt_required, create_access_token
+from flask_restful import Resource
+import hashlib
+
+from modelos import db, Upload, UploadSchema, Usuario, UsuarioSchema
+
+upload_schema = UploadSchema()
+usuario_schema = UsuarioSchema()
+
+
+class VistaSignIn(Resource):
+
+    # solo el post hace parte de la entrega
+    def post(self):
+        usuario = Usuario.query.filter(Usuario.correo == request.json["correo"]).first()
+        if usuario is None:
+            contrasena_encriptada = hashlib.md5(request.json["contrasena"].encode('utf-8')).hexdigest()
+            nuevo_usuario = Usuario(usuario_nombre=request.json["usuario_nombre"], contrasena=contrasena_encriptada,
+                                    correo=request.json["correo"])
+            db.session.add(nuevo_usuario)
+            db.session.commit()
+            token_de_acceso = create_access_token(identity=nuevo_usuario.id)
+            return {"mensaje": "usuario creado exitosamente", "id": nuevo_usuario.id}
+        else:
+            return "El usuario ya existe", 404
+
+    def put(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        usuario.contrasena = request.json.get("contrasena", usuario.contrasena)
+        usuario.correo = request.json.get("correo", usuario.correo)
+        db.session.commit()
+        return usuario_schema.dump(usuario)
+
+    def delete(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        db.session.delete(usuario)
+        db.session.commit()
+        return '', 204
+
+
+class VistaLogIn(Resource):
+
+    def post(self):
+
+        contrasena_encriptada = hashlib.md5(request.json["contrasena"].encode('utf-8')).hexdigest()
+        correo = Usuario.query.filter(Usuario.correo == request.json["correo"],
+                                      Usuario.contrasena == contrasena_encriptada).first()
+        db.session.commit()
+        if correo is None:
+            return "Error de Autenticación: El usuario no existe en el sistema", 404
+        else:
+            token_de_acceso = create_access_token(identity=correo.id)
+            return {"mensaje": "Inicio de sesión exitoso", "token": token_de_acceso, "id": correo.id}
+
+
+class VistaTasks(Resource):
+
+    def post(self):
+        file = request.files['file']
+        upload = Upload(nombre_archivo=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+        filename = file.filename
+        return {"File uploaded=": filename}
+
+    def get(self):
+        return None
+
+class VistaTaskId(Resource):
+    def get(self):
+        return None
+
+    def delete(self):
+        return None
+
+class VistaFiles(Resource):
+    def get(self):
+        return None
+
+class VistaUsuarios(Resource):
+    @jwt_required()
+    def get(self):
+        return [usuario_schema.dump(usuario) for usuario in Usuario.query.all()]
+
+    @jwt_required()
+    def post(self):
+        nuevo_usuario = Usuario(usuario_nombre=request.json['usuario_nombre'], \
+                                contrasena=request.json['contrasena'], \
+                                correo=request.json['correo'])
+        db.session.add(nuevo_usuario)
+        db.session.commit()
+        return usuario_schema.dump(nuevo_usuario)
+
+
+class VistaUsuario(Resource):
+    @jwt_required()
+    def get(self, id_usuario):
+        return usuario_schema.dump(Usuario.query.get_or_404(id_usuario))
+
+    @jwt_required()
+    def put(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        usuario.usuario_nombre = request.json.get('usuario_nombre', usuario.usuario_nombre)
+        usuario.contrasena = request.json.get('contrasena', usuario.contrasena)
+        usuario.correo = request.json.get('correo', usuario.correo)
+        db.session.commit()
+        return usuario_schema.dump(usuario)
+
+    @jwt_required()
+    def delete(self, id_usuario):
+        usuario = Usuario.query.get_or_404(id_usuario)
+        db.session.delete(usuario)
+        db.session.commit()
+        return 'Operación exitosa', 204
